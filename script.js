74aad6eefbcb75116d1e70fe895771dfcd8aa475
Yasmin <yasminlopesprojetos@gmail.com>
2026-02-10 08:41:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//exercicio 1
+let meunome="Yasmin";
+let minhaidade="17"
+console.log(meunome,minhaidade);
+//exercicio 2
+let comidafavorita="Strogonoff";
+comida="feijoada";
+//const paisNascimento=Brasil;
+//paisNascimento=EUA;
+//console.log(paisNascimento);
+//exercicio 3
+let nome1="Yasmin";
+let idade1 ="17"
+let cidade="Guarulhos"
+console.log(nome1,idade1,cidade);
+//exercicio 4
+let curso="DS I2";
+curso="Moda"
+console.log(curso);
+//exercicio 5
+//const anoAtual= 2026;
+//anoAtual=2014;
+//console.log(anoAtual);
+//exercicio 6
+let nome2="Yasmin";
+let sobreNome="Lopes";
+console.log(nome2,sobreNome)
+//exercicio 7
+let usuario="Yasmin";
+let email="yasminlopesprojetos@gmail.com"
+let idade2="17"
+console.log(usuario,email,idade2);
+//exercicio 8
+
+let numero1="17"
+let numero2="18"
+console.log(numero1,numero2);
+//exercicio 9
+let nota1="10"
+let nota2="5"
+let nota3="8"
+console.log(nota1,nota2,nota3);
+//exercicio 10
+let produto="shampoo"
+let preço="R$18,85"
+console.log(produto,preço);
